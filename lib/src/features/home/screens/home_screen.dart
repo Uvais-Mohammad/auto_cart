cae57093b2b612474bd15b0931d2003cd064b6a0
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 08:06:01 +0530

    Update home screen layout

--- lib/src/features/home/screens/home_screen.dart
+++ lib/src/features/home/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_cart/src/features/home/widgets/app_bar.dart';
-import 'package:auto_cart/src/features/home/widgets/primary_card.dart';
+import 'package:auto_cart/src/features/home/widgets/primary_section.dart';
+import 'package:auto_cart/src/features/home/widgets/product_category_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -14,30 +15,15 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        appBar: MyAppBar(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: PrimaryCard(
-                      title: 'Bookings',
-                      subtitle: 'View\nbookings',
-                      icon: Icons.book_online_outlined,
-                    ),
-                  ),
-                  Expanded(
-                    child: PrimaryCard(
-                      title: 'Offers',
-                      subtitle: 'Add/view\noffers',
-                      icon: Icons.local_offer_outlined,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ));
+      appBar: MyAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PrimarySection(),
+            ProductCategorySection(),
+          ],
+        ),
+      ),
+    );
   }
 }
