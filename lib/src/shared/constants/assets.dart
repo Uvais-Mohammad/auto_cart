1f6658461985c3d43f3c4a030c82bf0b40121c83
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 21:14:25 +0530

    Update assets and endpoints

--- lib/src/shared/constants/assets.dart
+++ lib/src/shared/constants/assets.dart
@@ -1,3 +1,2 @@
 class Assets {
-  static const String termsAndConditions = "assets/terms_and_conditions.json";
 }
--- lib/src/shared/constants/endpoints.dart
+++ lib/src/shared/constants/endpoints.dart
@@ -1,3 +1,5 @@
-class EndPoints{
-  static const String posts = "https://jsonplaceholder.typicode.com/posts";
+class EndPoints {
+  static const String baseUrl = 'https://roadmate.in/admin/public/api/';
+  static const String imageBaseUrl = 'https://roadmate.in/admin/public/market/';
+  static const String category = 'categorylist';
 }
