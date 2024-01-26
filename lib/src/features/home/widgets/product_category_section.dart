282458975b97bdbd4b6fab7ff2538e8c343b5ded
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 21:15:19 +0530

    Add category provider and screen

--- lib/src/features/home/widgets/product_category_section.dart
+++ lib/src/features/home/widgets/product_category_section.dart
@@ -1,4 +1,8 @@
+import 'package:auto_cart/src/features/home/logic/category/category_provider.dart';
+import 'package:auto_cart/src/features/home/models/category.dart';
+import 'package:auto_cart/src/features/home/screens/category_screen.dart';
 import 'package:auto_cart/src/features/home/widgets/category_card.dart';
+import 'package:auto_cart/src/shared/extensions/navigation_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,6 +10,8 @@ class ProductCategorySection extends ConsumerWidget {
   const ProductCategorySection({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    AsyncValue<List<ProductCategory>> categories =
+        ref.watch(productCategoryProvider);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -19,48 +25,56 @@ class ProductCategorySection extends ConsumerWidget {
         const SizedBox(height: 5),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 4),
-          child: Row(
-            children: [
-              Expanded(
-                child: SizedBox(
-                  height: 110,
-                  child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) =>
-                        const CategoryCard(icon: Icons.abc, title: 'title'),
+          child: categories.when(
+            data: (data) => Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 110,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (context, index) =>
+                          CategoryCard(category: data[index]),
+                    ),
                   ),
                 ),
-              ),
-              //view all button
-              SizedBox(
-                height: 110,
-                child: Card(
-                  margin: const EdgeInsets.fromLTRB(10, 8, 6, 8),
-                  child: InkWell(
-                    onTap: () {},
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                      child: Row(
-                        children: [
-                          Text(
-                            'View All',
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w600,
+                //view all button
+                SizedBox(
+                  height: 110,
+                  child: Card(
+                    margin: const EdgeInsets.fromLTRB(10, 8, 6, 8),
+                    child: InkWell(
+                      onTap: () {
+                        context.push(const CategoryScreen());
+                      },
+                      borderRadius: BorderRadius.circular(12),
+                      child: const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        child: Row(
+                          children: [
+                            Text(
+                              'View All',
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                          Icon(
-                            Icons.chevron_right_outlined,
-                            size: 18,
-                          ),
-                        ],
+                            Icon(
+                              Icons.chevron_right_outlined,
+                              size: 18,
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
+            loading: () => const Center(child: CircularProgressIndicator()),
+            error: (error, stackTrace) => Text(error.toString()),
           ),
         )
       ],
