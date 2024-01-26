ff0e2c8035fe5cd3e6d6b10ad09715d6370b48cc
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 21:14:12 +0530

    Add ProductCategory and Product models

---
+++ lib/src/features/home/models/category.dart
@@ -0,0 +1,40 @@
+class ProductCategory {
+  final int id;
+  final String categoryName;
+  final int catId;
+  final String image;
+  final String createdAt;
+  final String updatedAt;
+  final int status;
+
+  const ProductCategory({
+    required this.id,
+    required this.categoryName,
+    required this.catId,
+    required this.image,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.status,
+  });
+
+  ProductCategory.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        categoryName = json['category_name'],
+        catId = json['cat_id'],
+        image = json['image'],
+        createdAt = json['created_at'],
+        updatedAt = json['updated_at'],
+        status = json['status'];
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['category_name'] = categoryName;
+    data['cat_id'] = catId;
+    data['image'] = image;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    data['status'] = status;
+    return data;
+  }
+}
---
+++ lib/src/features/home/models/product.dart
@@ -0,0 +1,64 @@
+class Product {
+  final int? id;
+  final int? brandId;
+  final String? productName;
+  final int? offerPrice;
+  final int? price;
+  final String? description;
+  final int? status;
+  final int? hsncode;
+  final int? priority;
+  final String? createdAt;
+  final String? updatedAt;
+  final int? catId;
+  final List<String>? images;
+
+  const Product({
+    this.id,
+    this.brandId,
+    this.productName,
+    this.offerPrice,
+    this.price,
+    this.description,
+    this.status,
+    this.hsncode,
+    this.priority,
+    this.createdAt,
+    this.updatedAt,
+    this.catId,
+    this.images,
+  });
+
+  Product.fromJson(Map<String, dynamic> json)
+      : id = json['id'] as int?,
+        brandId = json['brand_id'] as int?,
+        productName = json['product_name'] as String?,
+        offerPrice = json['offer_price'] as int?,
+        price = json['price'] as int?,
+        description = json['description'] as String?,
+        status = json['status'] as int?,
+        hsncode = json['hsncode'] as int?,
+        priority = json['priority'] as int?,
+        createdAt = json['created_at'] as String?,
+        updatedAt = json['updated_at'] as String?,
+        catId = json['cat_id'] as int?,
+        images = json['images'] as List<String>?;
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['brand_id'] = brandId;
+    data['product_name'] = productName;
+    data['offer_price'] = offerPrice;
+    data['price'] = price;
+    data['description'] = description;
+    data['status'] = status;
+    data['hsncode'] = hsncode;
+    data['priority'] = priority;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    data['cat_id'] = catId;
+    data['images'] = images;
+    return data;
+  }
+}
