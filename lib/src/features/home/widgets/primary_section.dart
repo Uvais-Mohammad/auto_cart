3c46f2a817cdd64167ccc0fbea97410f603b4134
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 07:41:47 +0530

    Add PrimarySection widget

---
+++ lib/src/features/home/widgets/primary_section.dart
@@ -0,0 +1,34 @@
+import 'package:auto_cart/src/features/home/widgets/primary_card.dart';
+import 'package:flutter/material.dart';
+
+class PrimarySection extends StatelessWidget {
+  const PrimarySection({
+    super.key,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Expanded(
+            child: PrimaryCard(
+              title: 'Bookings',
+              subtitle: 'View\nbookings',
+              icon: Icons.book_online_outlined,
+            ),
+          ),
+          Expanded(
+            child: PrimaryCard(
+              title: 'Offers',
+              subtitle: 'Add/view\noffers',
+              icon: Icons.local_offer_outlined,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
