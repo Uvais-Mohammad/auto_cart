d5ca258c5b2664bbf693006254d67dcd3eb036b2
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-26 21:14:44 +0530

    Add featured section, bottom bar, and products button to home screen

--- lib/src/features/home/screens/home_screen.dart
+++ lib/src/features/home/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:auto_cart/src/features/home/widgets/app_bar.dart';
+import 'package:auto_cart/src/features/home/widgets/featured_section.dart';
+import 'package:auto_cart/src/features/home/widgets/my_bottom_bar.dart';
 import 'package:auto_cart/src/features/home/widgets/primary_section.dart';
 import 'package:auto_cart/src/features/home/widgets/product_category_section.dart';
+import 'package:auto_cart/src/features/home/widgets/products_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,14 +19,16 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: MyAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            PrimarySection(),
-            ProductCategorySection(),
-          ],
-        ),
+      body: Column(
+        children: [
+          PrimarySection(),
+          ProductCategorySection(),
+          FeaturedProductSection(),
+        ],
       ),
+      bottomNavigationBar: MyBottomBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: ProductsButton(),
     );
   }
 }
