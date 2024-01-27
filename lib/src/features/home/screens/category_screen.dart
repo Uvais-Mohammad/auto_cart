50dddb24eeeb51325842974a21b895f6c60e4a94
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-27 21:42:51 +0530

    Refactor category screen and home screen widgets

--- lib/src/features/home/screens/category_screen.dart
+++ lib/src/features/home/screens/category_screen.dart
@@ -1,6 +1,8 @@
 import 'package:auto_cart/src/features/home/logic/category/category_provider.dart';
 import 'package:auto_cart/src/features/home/models/category.dart';
-import 'package:auto_cart/src/features/home/widgets/category_card.dart';
+import 'package:auto_cart/src/features/home/screens/product_screen.dart';
+import 'package:auto_cart/src/features/home/widgets/category/category_card.dart';
+import 'package:auto_cart/src/shared/extensions/navigation_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -31,30 +33,45 @@ class _CategoryScreenState extends ConsumerState<CategoryScreen> {
         ],
       ),
       body: categories.when(
-        data: (data) => Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Expanded(
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    childAspectRatio: 0.8,
-                  ),
-                  itemCount: data.length,
-                  itemBuilder: (context, index) =>
-                      CategoryCard(category: data[index]),
+        data: (data) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        childAspectRatio: 1.2,
+                      ),
+                      itemCount: data.length - 1,
+                      itemBuilder: (context, index) {
+                        return CategoryCard(
+                          category: data[index + 1],
+                          isAllCategory: true,
+                          onTap: () {
+                            ref.read(selectedCategoryProvider.notifier).state =
+                                data[index + 1];
+                            context.push(const ProductScreen());
+                          },
+                        );
+                      }),
                 ),
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: const Text('View All Products'),
-              ),
-              const Spacer(),
-            ],
-          ),
-        ),
+                OutlinedButton(
+                  onPressed: () {
+                    ref.read(selectedCategoryProvider.notifier).state =
+                        data.first;
+                    context.push(const ProductScreen());
+                  },
+                  child: const Text('View All Products'),
+                ),
+                const Spacer(),
+              ],
+            ),
+          );
+        },
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (error, stack) => Center(child: Text(error.toString())),
       ),
--- lib/src/features/home/screens/home_screen.dart
+++ lib/src/features/home/screens/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:auto_cart/src/features/home/widgets/app_bar.dart';
-import 'package:auto_cart/src/features/home/widgets/featured_section.dart';
+import 'package:auto_cart/src/features/home/widgets/product/featured_section.dart';
 import 'package:auto_cart/src/features/home/widgets/my_bottom_bar.dart';
 import 'package:auto_cart/src/features/home/widgets/primary_section.dart';
-import 'package:auto_cart/src/features/home/widgets/product_category_section.dart';
-import 'package:auto_cart/src/features/home/widgets/products_button.dart';
+import 'package:auto_cart/src/features/home/widgets/category/product_category_section.dart';
+import 'package:auto_cart/src/features/home/widgets/product/products_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
