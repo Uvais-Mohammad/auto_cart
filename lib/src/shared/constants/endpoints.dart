d8f0b6c72b3447935f7ac700177c5503a4e8e964
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-27 21:41:39 +0530

    Update endpoints in constants file

--- lib/src/shared/constants/endpoints.dart
+++ lib/src/shared/constants/endpoints.dart
@@ -1,6 +1,12 @@
 class EndPoints {
   static const String baseUrl = 'https://roadmate.in/admin/public/api/';
+
   static const String imageBaseUrl = 'https://roadmate.in/admin/public/market/';
   static const String category = 'categorylist';
-  static const String product = 'product';
+  static const String subCategory = 'subcategorylist';
+  static const String product =
+      'https://roadmate.in/mechine_test/public/api/product';
+  static const String productByCategory =
+      'https://roadmate.in/mechine_test/public/api/categoryproductlist';
+  static const String brand = "brand_list";
 }
