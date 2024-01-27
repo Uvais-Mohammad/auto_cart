d40b52f5bfabfa8caf1871b1ca7fecb523dba9ae
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-27 12:51:59 +0530

    Add product endpoint

--- lib/src/shared/constants/endpoints.dart
+++ lib/src/shared/constants/endpoints.dart
@@ -2,4 +2,5 @@ class EndPoints {
   static const String baseUrl = 'https://roadmate.in/admin/public/api/';
   static const String imageBaseUrl = 'https://roadmate.in/admin/public/market/';
   static const String category = 'categorylist';
+  static const String product = 'product';
 }
