7219abfd812034258fbb958d85c21de01628194d
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-27 21:41:15 +0530

    Add category, subcategory, and brand providers

--- lib/src/features/home/logic/category/category_provider.dart
+++ lib/src/features/home/logic/category/category_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:auto_cart/src/features/home/models/brand.dart';
 import 'package:auto_cart/src/features/home/models/category.dart';
 import 'package:auto_cart/src/features/home/repository/home_repo.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -7,6 +10,75 @@ final productCategoryProvider =
   return ref.read(homeRepositoryProvider).getCategories();
 });
 
-final selectedCategoryProvider = StateProvider<ProductCategory?>((ref) {
-  return null;
+final selectedCategoryProvider = StateProvider<ProductCategory>((ref) {
+  return const ProductCategory(
+    id: 0,
+    categoryName: 'All Products',
+  );
 });
+
+final subCategoryProvider =
+    AsyncNotifierProvider<SubCategoryProvider, List<ProductCategory>>(
+        SubCategoryProvider.new);
+
+final selectedSubCategoryProvider = StateProvider<ProductCategory>((ref) {
+  return const ProductCategory(
+    id: 0,
+    categoryName: 'All',
+  );
+});
+
+final brandProvider = AsyncNotifierProvider<BrandProvider, List<Brand>>(
+    BrandProvider.new);
+
+final selectedBrandProvider = StateProvider<List<Brand>>((ref) {
+  return [];
+});
+
+class SubCategoryProvider extends AsyncNotifier<List<ProductCategory>> {
+  @override
+  Future<List<ProductCategory>> build() async {
+    final catId = ref.read(selectedCategoryProvider.notifier).state.id;
+    return await _loadData(catId);
+  }
+
+  FutureOr<List<ProductCategory>> _loadData(int index) {
+    if (index == 0) {
+      return [];
+    }
+    return ref.read(homeRepositoryProvider).getSubCategories(index);
+  }
+
+  Future<void> refresh() async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async {
+        final catId = ref.read(selectedCategoryProvider.notifier).state.id;
+        return _loadData(catId);
+      },
+    );
+  }
+}
+
+class BrandProvider extends AsyncNotifier<List<Brand>> {
+  @override
+  Future<List<Brand>> build() async {
+    final subCatId = ref.read(selectedSubCategoryProvider.notifier).state.id;
+    return await _loadData(subCatId);
+  }
+
+  FutureOr<List<Brand>> _loadData(int subCatId) {
+    return ref.read(homeRepositoryProvider).getBrands(subCatId);
+  }
+
+  Future<void> refresh() async {
+    state = const AsyncValue.loading();
+    state = await AsyncValue.guard(
+      () async {
+        final subCatId =
+            ref.read(selectedSubCategoryProvider.notifier).state.id;
+        return _loadData(subCatId);
+      },
+    );
+  }
+}
