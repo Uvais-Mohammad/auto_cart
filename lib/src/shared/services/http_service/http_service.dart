b7492e305063ab5abfd4057f7686452333439887
Uvais-Mohammad <uvaiskc55@gmail.com>
2024-01-27 21:40:20 +0530

    Add HttpService implementation

--- lib/src/shared/services/http_service/http_service.dart
+++ lib/src/shared/services/http_service/http_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:auto_cart/src/shared/services/http_service/i_http_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
@@ -10,5 +12,13 @@ final class HttpService implements IHttpService {
   @override
   Future<http.Response> get(String url) async {
     return http.get(Uri.parse(url));
+  }
+
+  @override
+  Future<http.Response> post(String url, {Map<String, dynamic>? body}) async {
+    return http.post(
+      Uri.parse(url),
+      body: jsonEncode(body)
+    );
   }
 }
--- lib/src/shared/services/http_service/i_http_service.dart
+++ lib/src/shared/services/http_service/i_http_service.dart
@@ -2,4 +2,5 @@ import 'package:http/http.dart';
 
 abstract interface class IHttpService {
   Future<Response> get(String url);
+  Future<Response> post(String url, {Map<String, dynamic>? body});
 }
